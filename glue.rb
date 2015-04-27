3abfddadea848b02129120e10d08ab58222fdcd3
Cathy Cullen <cathy@softwareoptions.com>
2015-04-27 11:35:11 -0500

    add root route

--- glue.rb
+++ glue.rb
@@ -39,6 +39,10 @@ use Rack::Protection::HttpOrigin, :origin_whitelist => ['http://jodymichael.com'
                                                         'http://careercheetah.net',
                                                         'http://localhost:8000']
 
+
+get '/'
+'Hello from jma-glue'
+
 # Note: These endpoints should all be 'post', not 'get' to handle a form submission.
 # Left as 'get' for easy testing until we deploy.
 get '/constant_contact' do
